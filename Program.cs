eb5714353386343bc937a54a368c4ae295f748e2
namratachavan24 <namrata2015.nc@gmail.com>
2022-08-10 16:05:28 +0530

    Logical Practice Problem Added

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LogicalProblems
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            // PerfectNumber.PerfectNumProgram();
+            // PrimeNumber.PrimeNumberProgram();
+            // ReverseNumber.ReverseNumProgram();
+            //StopWatchProgram.StopWatch();
+            //StopWatchProgram.StopwatchCalculation();
+            //ponProgram.CouponNumbers();
+            FibbonacciSeries.DisplayFibboSeries();
+            NonNegativeNum.Display();
+        }
+    }
+}
