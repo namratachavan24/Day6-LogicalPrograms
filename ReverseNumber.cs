7ba97025181abc13693bc10895408f2a2b62af67
namratachavan24 <109748579+namratachavan24@users.noreply.github.com>
2022-08-10 16:14:27 +0530

    Day-6 Practice Problem Added

---
+++ ReverseNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    public class ReverseNumber
+    {
+        public static void ReverseNumProgram()
+        {
+            int number, reverse = 0;
+
+            Console.WriteLine("Enter a Number :");
+            number = int.Parse(Console.ReadLine());
+
+            while (number != 0)
+            {
+                reverse = reverse * 10;
+                reverse = reverse + number % 10;
+                number = number / 10;
+            }
+            Console.WriteLine("Reverse of entered Number is : " + reverse);
+        }
+    }
+}
