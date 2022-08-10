e9824bbe52318cedc3c121758ac3fc5092b73135
namratachavan24 <109748579+namratachavan24@users.noreply.github.com>
2022-08-10 16:15:15 +0530

    Day-6 Practice Problem Added

---
+++ StopWatchProgram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    static class StopWatchProgram
+    {
+        public static void StopwatchCalculation()
+        {
+            Stopwatch watch = new Stopwatch();
+
+            int max = 10000000;
+            Random random = new Random();
+            int randomLap = random.Next(0, max);
+            for (int i = 0; i < max; i++)
+            {
+                watch.Start();
+                if (i == randomLap)
+                {
+                    randomLap = random.Next(0, max);
+                    watch.Stop();
+                    TimeSpan ts = watch.Elapsed;
+                    Console.WriteLine("\n\nTime Lap: " + ts);
+                }
+            }
+        }
+    }
+}
