ff353522a207f4e86c93666725eaf711eb727ea0
namratachavan24 <namrata2015.nc@gmail.com>
2022-08-10 16:11:55 +0530

    Day-6 Practice Problem Added

---
+++ PrimeNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    public class PrimeNumber
+    {
+        public static void PrimeNumberProgram()
+        {
+            Console.WriteLine("Enter the Number: \n");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            bool isPrime = true;
+
+            for (int i = 2; i < number/2; i++)
+            {
+                if (number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime)
+            {
+                Console.WriteLine("Number is Prime");
+            }
+            else
+            {
+                Console.WriteLine("Number is not Prime");
+            }
+        }
+    }
+}
